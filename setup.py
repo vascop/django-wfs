50662494ae653426421a60b96d1df0fa9c49866e
Wolfgang Glas <wolfgang.glas@iteg.at>
2016-10-31 14:49:14 +0100

    Adapt to django-1.10.x and set version to 0.2.1-0 for next development cycle.

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import setup, find_packages
+import os
+import re
 
+PACKAGE="django-wfs"
+
+MYDIR = os.path.dirname(__file__)
+
+def read_version():
+    fn  = os.path.join(os.path.join(MYDIR,"debian-unix"),"changelog")
+    with open(fn) as fd:
+        line = fd.readline()   
+        version,n = re.subn('^'+PACKAGE+'\\s*\\(([^-]*)-[^)]\\).*\n','\\1',line)
+        if n != 1:
+            raise SyntaxError("debian changelog line [%s] is malformatted"%line.substring[:-1])
+        return version
 
 setup(
-    name='django-wfs',
+    name=PACKAGE,
     packages=find_packages(),
     include_package_data=True,
     package_dir={'wfs': 'wfs'},
-    version='0.2.0',
+    version=read_version(),
     description='A WFS (web feature service) implementation as a Django application.',
     author='Vasco Pinho',
     author_email='vascogpinho@gmail.com',
--- wfs/urls.py
+++ wfs/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from wfs.views import global_handler
 
 # APP
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^(?P<service_id>\d+)/$', global_handler, name='wfs'),
-)
+]
